1f2437110cfc8d941df8fcc7843389f04ad7a629
Muhammad Haikal <realityinaship@gmail.com>
2021-04-07 15:09:28 +0700

    Added main function

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
 def greet_human_only(name):
     return f"Hello {name}!" if name.isalpha() else "No greeting for you."
 
-if __name__ == '__main__':
+def main():
     print(greet_human_only(input("Input your name: ")))
+
+
+if __name__ == '__main__':
+    main()
